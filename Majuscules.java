cd5bee325b7445c230d9144ca0613e5be50b7f05
Jordi Ribellas <ribellas2002@gmail.com>
2022-11-10 16:09:07 +0100

    fix

--- Majuscules.java
+++ Majuscules.java
@@ -6,16 +6,15 @@ import java.io.PrintStream;
 public class Majuscules{
 
     public static void main(String[] args) {
-        String line;
+        String line="";
         try {
+            while(line.compareTo("fin")!=0) {
             Process son= new ProcessBuilder("java", "-jar", "C:\\Users\\Jordi\\Desktop\\DAM\\M9\\Exercicis\\Act1\\out\\artifacts\\Act1_jar\\Act1.jar"
                     + "").start();
             BufferedReader br=new BufferedReader(new InputStreamReader(son.getInputStream()));
             PrintStream ps=new PrintStream(son.getOutputStream());
 
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            line = in.readLine();
-            while(line.compareTo("fin")!=0) {
                 ps.println(line);
                 ps.flush();
                 if((line=br.readLine())!=null) {
